f90dc72589c4f19fcfa3aa6a83da38b52bafe44c
NanaTrisna <trisna.gumilang420@gmail.com>
2021-02-12 01:26:53 +0700

    terbaru

--- include/sql.sql
+++ include/sql.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 11 Feb 2021 pada 05.28
--- Versi server: 10.4.17-MariaDB
--- Versi PHP: 7.4.13
+-- Waktu pembuatan: 11 Feb 2021 pada 19.23
+-- Versi server: 10.4.6-MariaDB
+-- Versi PHP: 7.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -42,7 +43,7 @@ CREATE TABLE `aboutus_setup` (
 --
 
 INSERT INTO `aboutus_setup` (`id`, `shortdesc`, `heading`, `subheading`, `longdesc`, `website`, `dob`) VALUES
-(1, 'Nama lengkap saya Fariz Hawari Sugiarto, ber Agama Islam dan saya \r\nsekarang berumur 20 tahun. Saya memiliki kondisi kesehatan yang \r\nsangat baik dan mampu berkomunikasi dengan bahasa indonesia \r\nyang baik dan sopan. Saya adalah seorang mahasiswa yang memiliki\r\nKemauan untuk mempelajari hal-hal baru. Saya mampu bekerja \r\nsecara mandiri maupun dalam tim dengan baik,', 'Kreatif Post / Web Developer', '', '            ', '', '29, Oktober 2000');
+(1, '<p align=\"justify\"><b>SCIMMIASKA</b> Adalah Band asal Bandung yang bergenrekan <b>SKA ROCKSTEADY</b>, Band yang bergenre SKA Rock Steady ini lahir pada awal bulan Juli 2012 di Batujajar, Bandung Barat. ScimmiaSka terbentuk bermula dari keinginan salah satu personilnya untuk membuat salah satu band beraliaran musik SKA dan ikut membaur di komunitas musik SKA Bandung dan Indonesia pada umumnya dengan formasi awal Senny (vocal), Reza (trumpet), Giant (keyboard), Anggi (bass), Jimi (guitar), Onie (gitar) dan Yogi (drum)</p>', 'SEJARAH DAN DESKRIPSI', '', '<p align=\"justify\"><b>SCIMMIASKA</b> bisa terus eksis sampai sekarang karena dukungan pemirsa dan para pecinta musik yang terus mendukung <b>SCIMMIASKA</b>, khususnya keluarga besar <b>SCIMMIASKA</b>, yang umum dikenal dengan sebutan Keluarga besar <b>SCIMMIAMI.INDONESIA</b>, yang terus memberikan support yang sangat besar kepada <b>SCIMMIASKA</b>. selain itu bentuk suport <b>SCIMMIAMI.INDONESIA</b> sangatlah besar bahkan ketika ada EVENT musik di daerah manapun <b>SCIMMIASKA</b> manggung mereka selalu datang untuk mensuport Band Kesayangan mereka, dan saya yang membuat WEBSITE inipun merukapan salah satu bagian dari kerluarga besar <b>SCIMMIAMI.INDONESIA</b>. <br>\r\n       \r\n                          ', 'https://www.reverbnation.com/scimmiaska', '12, Juli 2012');
 
 -- --------------------------------------------------------
 
@@ -63,7 +64,7 @@ CREATE TABLE `admin_users` (
 --
 
 INSERT INTO `admin_users` (`id`, `username`, `user_id`, `user_pass`, `user_access`) VALUES
-(1, 'Admin', 'admin@admin.com', 'admin', 1);
+(1, 'TrisnaGumilang', 'trisna.gumilang420@gmail.com', 'gumilang', 1);
 
 -- --------------------------------------------------------
 
@@ -84,7 +85,7 @@ CREATE TABLE `basic_setup` (
 --
 
 INSERT INTO `basic_setup` (`id`, `title`, `description`, `keyword`, `icon`) VALUES
-(1, 'Fariz Hawari', 'Website Portfolio ini dibuat untuk memenuhi tugas kuliah:)', 'Mahasiswa,Programer,Media Creator', '7060718_preview.png');
+(1, 'SCIMMIASKA_INDONESIA', 'Website Scimmiaska', 'Band Favorit', 'tampilan.png');
 
 -- --------------------------------------------------------
 
@@ -105,8 +106,7 @@ CREATE TABLE `contact` (
 --
 
 INSERT INTO `contact` (`id`, `cname`, `cemail`, `csubject`, `cmessage`) VALUES
-(18, 'Fariz Hawari', 'fariz.sco.77@gmail.com', 'CV Fariz', 'Hallo nama saya Fariz HAwari bla bla bla'),
-(20, 'Nana', 'nanatrisna@gmail.com', 'Tugas', 'Assalammualaikum nih tugas bangsat');
+(21, 'TrisnaGumilang', 'trisna.gumilang420@gmail.com', 'Booking Performance', 'Booking untuk tanggal 20 Februari 2021 di lapangan Majalaya');
 
 -- --------------------------------------------------------
 
@@ -136,7 +136,7 @@ CREATE TABLE `personal_setup` (
 --
 
 INSERT INTO `personal_setup` (`id`, `profilepic`, `name`, `twitter`, `facebook`, `instagram`, `skype`, `linkedin`, `github`, `homewallpaper`, `professions`, `location`, `mobile`, `emailid`) VALUES
-(1, 'nih.jpg', 'Fariz Hawari', '', 'https://www.facebook.com/fariz.SCO.77', 'https://www.instagram.com/vestolaer', '', '', 'https://github.com/FarizHaw/', 'samuel-scalzo-xyuYk9oLA8I-unsplash.jpg', 'Mahasiswa,Programer,Media Creator', 'Jl. Babakan Sumedang, Komplek PosGiro 05/05 Kab.Bandung', '+6289630908640', 'farizh77@gmail.com');
+(1, 'tampilan.png', 'SCIMMIASKA <br> INDONESIA', '', '', 'https://www.instagram.com/_nanatrisna', '', 'https://www.reverbnation.com/scimmiaska', 'https://github.com/NanaTrisna', 'img-18.jpeg', 'BANDUNG SKA ROCKSTEADY,WE ARE NOT FANS-WE ARE FAMILY', 'Jl. Balekambang Dangdeur Kec.Majalaya Des.Sukamaju Kab.Bandung', '+6281224617418', 'trisna.gumilang420@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -156,12 +156,21 @@ CREATE TABLE `portfolio` (
 --
 
 INSERT INTO `portfolio` (`id`, `projectname`, `projectpic`, `projectlink`) VALUES
-(8, '1', '1.jpg', '#'),
-(9, '2', '2.jpg', '#'),
-(10, '3', '3.jpg', '#'),
-(11, '4', '4.jpg', '#'),
-(12, '5', '5.jpg', '#'),
-(14, '6', '6.jpg', '#');
+(15, '', 'img-1.jpeg', '#'),
+(16, '', 'img-2.jpeg', ''),
+(17, '', 'img-3.jpeg', ''),
+(18, '', 'img-23.jpeg', ''),
+(19, '', 'img-21.jpeg', ''),
+(21, '', 'img-4.jpeg', ''),
+(23, '', 'img-11.jpeg', ''),
+(24, '', 'img-13.jpeg', ''),
+(25, '', 'img-9.jpeg', ''),
+(30, '', 'IMG_20180422_104859_374.jpg', ''),
+(31, '', 'img-20.jpeg', ''),
+(33, '', 'img-25.jpeg', ''),
+(34, '', 'img-19.jpeg', ''),
+(35, '', 'img-18.jpeg', ''),
+(36, '', 'img-10.jpeg', '');
 
 -- --------------------------------------------------------
 
@@ -183,12 +192,27 @@ CREATE TABLE `resume` (
 --
 
 INSERT INTO `resume` (`id`, `category`, `title`, `year`, `ogname`, `workdesc`) VALUES
-(10, 'e', 'Sekolah Dasar', '2006-2012', 'SDN Sejahtera IV Bandung', ''),
-(11, 'e', 'Sekolah Menengah Pertama', '2012-2015', 'SMPN 46 Bandung', ''),
-(12, 'e', 'Sekolah Menengah Kejuruan', '2015-2018', 'SMK Informatika Bandung', 'Jurusan Rekayasa Perangkat Lunak (RPL)'),
-(13, 'pe', 'Magang', '2018', 'Percetakan Ijazah', ''),
-(14, 'pe', 'Freelance', '2017-2020', 'Desain Banner & Digital Wedding', ''),
-(15, 'pe', 'Freelance', '2019-Sekarang', 'WR Project, RAB Photography', '');
+(16, 'e', 'Vokalis', 'Givani Gumilang', 'SCIMMIASKA BANDUNG SKA', 'Vokalis Scimmiaska Terbaik sepanjang masa'),
+(17, 'pe', 'Intro (Alba)', 'Album - Alba', 'SCIMMIASKA BANDUNG SKA', ''),
+(18, 'e', 'Pemain Keyboard', 'Gian (Iqbal Giantaufik)', 'SCIMMIASKA BANDUNG SKA', ''),
+(19, 'e', 'Pemain Trumpet', 'Eot (Reza Dwi Nugraha)', 'SCIMMIASKA BANDUNG SKA', ''),
+(20, 'e', 'Pemain Saksofon', 'Asan (Shandra Adhiman Sobari)', 'SCIMMIASKA BANDUNG SKA', ''),
+(21, 'e', 'Pemain Saksofon', 'Reza (Reza Surya Pradina)', 'SCIMMIASKA BANDUNG SKA', ''),
+(22, 'e', 'Gitaris', 'Oni (Roni Riansyah)', 'SCIMMIASKA BANDUNG SKA', ''),
+(23, 'e', 'Gitaris', 'Jim (Jim Desta Firdaus)', 'SCIMMIASKA BANDUNG SKA', ''),
+(24, 'e', 'Pemain Bass', 'Jue (Anggi Fichriadi)', 'SCIMMIASKA BANDUNG SKA', ''),
+(25, 'e', 'Pemain Trombone', 'Ogi (Yoghi Adhia Prasandi)', 'SCIMMIASKA BANDUNG SKA', ''),
+(26, 'e', 'Pemain Drum', 'Ogi (Yoghi Adhia Prasandi) & Erik (Erick Mochamad Ilyas)', 'SCIMMIASKA BANDUNG SKA', ''),
+(27, 'pe', 'Bebaskan', 'Album - Alba', 'SCIMMIASKA BANDUNG SKA', ''),
+(28, 'pe', 'Lembayung', 'Album - Alba', 'SCIMMIASKA BANDUNG SKA', ''),
+(29, 'pe', 'Berkumpul Bersama', 'Album - Alba', 'SCIMMIASKA BANDUNG SKA', ''),
+(30, 'pe', 'Lelah', 'Album - Alba', 'SCIMMIASKA BANDUNG SKA', ''),
+(31, 'pe', 'Biarkan', 'Album - Alba', 'SCIMMIASKA BANDUNG SKA', ''),
+(32, 'pe', 'No Problem', 'Album - Alba', 'SCIMMIASKA BANDUNG SKA', ''),
+(33, 'pe', 'With You', 'Album - Alba', 'SCIMMIASKA BANDUNG SKA', ''),
+(34, 'pe', 'Dengarkan', 'Album - Alba', 'SCIMMIASKA BANDUNG SKA', ''),
+(35, 'pe', 'You Dance With Me', 'Album - Alba', 'SCIMMIASKA BANDUNG SKA', ''),
+(36, 'pe', 'Kecewa', 'Album - Alba', 'SCIMMIASKA BANDUNG SKA', '');
 
 -- --------------------------------------------------------
 
@@ -207,14 +231,9 @@ CREATE TABLE `skills` (
 --
 
 INSERT INTO `skills` (`id`, `skill`, `score`) VALUES
-(12, 'HTML', '100'),
-(13, 'CSS', '90'),
-(14, 'PHP', '75'),
-(15, 'MYSQL', '75'),
-(16, 'Javascript', '60'),
-(17, 'Photoshop', '90'),
-(18, 'Bootstrap', '55'),
-(19, 'MS Word', '90');
+(20, 'Profesionalitas', '100'),
+(21, 'Existensi', '90'),
+(22, 'Fan Based', '90');
 
 --
 -- Indexes for dumped tables
@@ -294,7 +313,7 @@ ALTER TABLE `basic_setup`
 -- AUTO_INCREMENT untuk tabel `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT untuk tabel `personal_setup`
@@ -306,19 +325,19 @@ ALTER TABLE `personal_setup`
 -- AUTO_INCREMENT untuk tabel `portfolio`
 --
 ALTER TABLE `portfolio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT untuk tabel `resume`
 --
 ALTER TABLE `resume`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT untuk tabel `skills`
 --
 ALTER TABLE `skills`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
